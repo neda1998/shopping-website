831d783bb7893ec42739ec7181f4e7bbd785ce0d
Neda Gholami <90248679+neda1998@users.noreply.github.com>
2023-06-01 14:35:21 +0330

    change styles

--- src/Components/Tabs/Tabs.jsx
+++ src/Components/Tabs/Tabs.jsx
@@ -4,7 +4,7 @@ import TabComp from "./TabComp"
 
 function Tabs() {
   return (
-    <div className="md:px-20 px-10 w-full">
+    <div className="md:px-20 px-8 w-full">
      <div className={`${style.tab}`}>
       <div className={`py-20`}>
      <h1>Start exploring.</h1>
