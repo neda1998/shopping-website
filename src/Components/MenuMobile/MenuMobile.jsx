f6b0dcce1bf5acb931e6366daa18e7fa13ee6f3c
Neda Gholami <90248679+neda1998@users.noreply.github.com>
2023-06-10 12:10:36 +0330

    change link tag to p tag

--- src/Components/MenuMobile/MenuMobile.jsx
+++ src/Components/MenuMobile/MenuMobile.jsx
@@ -50,10 +50,10 @@ function MenuMobile({ handleNav }) {
                     <input type="text" placeholder="Type and press enter" />
                 </div>
                 <div className={style.itemsMenu}>
-                    <Link to="/men">MEN</Link>
-                    <Link to="/women">WOMEN</Link>
-                   <Link to="/beauty">BEAUTY</Link>
-                    <Link to="/sport">SPORT</Link>
+                    <p>MEN</p>
+                    <p>WOMEN</p>
+                   <p>BEAUTY</p>
+                    <p>SPORT</p>
                     <DropdownMobile />
                 </div>
                 <div className={style.btnMenuMobile}>
